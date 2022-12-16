eee4d1a3152b7c1444fe51320b56b70fcec836e5
yongsin0129 <yongsin0129@gmail.com>
2022-12-16 08:36:04 +0800

    feat : post 按讚功能

--- src/components/SinglePostPage.tsx
+++ src/components/SinglePostPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useParams,Link } from 'react-router-dom'
-import { useQuery, gql } from '@apollo/client'
+import { useParams, Link, useNavigate } from 'react-router-dom'
+import { useQuery, gql, useMutation } from '@apollo/client'
 
 const GET_SinglePost = gql`
   query PostDetail($postId: ID!) {
@@ -11,27 +11,71 @@ const GET_SinglePost = gql`
       author {
         name
       }
+      likeGivers {
+        id
+        name
+      }
     }
   }
 `
 
-const SinglePostPage = (props: any) => {
+const LIKE_POST = gql`
+  mutation ($postId: ID!) {
+    likePost(postId: $postId) {
+      id
+      title
+      body
+      author {
+        id
+        name
+      }
+      likeGivers {
+        id
+        name
+      }
+    }
+  }
+`
+const SinglePostPage = () => {
   let { id } = useParams()
+  const navigate = useNavigate()
+  const countVotes = (array: []) => array.length
 
-  const { loading, error, data } = useQuery(GET_SinglePost, {
+  const [LIKE_POST_Mutation, mutationRes] = useMutation(LIKE_POST)
+  // if (mutationRes.loading) return <p>mutation Loading...</p> // 因為 mutation 點下去也會 call useQuery 的 hook 所以不能有太多 return ?
+  // if (mutationRes.error) return <p>mutation Error </p>
+
+  const queryRes = useQuery(GET_SinglePost, {
     variables: { postId: id }
   })
-  if (loading) return <p>Loading...</p>
-  if (error) return <p>Error </p>
-  const { post } = data
+  if (queryRes.loading) return <p>query Loading...</p>
+  if (queryRes.error) return <p>query Error </p>
+  const { post } = queryRes.data
   const singlePage = (
     <div>
       <h3>title : {post.title}</h3>
-      <h6>id : {post.id}</h6>
+      <h6>post id : {post.id}</h6>
       <h6>body : {post.body}</h6>
       <h6>author : {post.author.name}</h6>
+      <hr />
+      <div>
+        <i
+          className='material-icons'
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            LIKE_POST_Mutation({
+              variables: {
+                postId: post.id
+              }
+            })
+          }}
+        >
+          thumb_up
+        </i>
+        <span>{countVotes(post.likeGivers)}</span>
+      </div>
       <br />
-      <Link to="/">Back</Link>
+      <Link to='/'>Back</Link>
     </div>
   )
 
